b92ec0c98f82dfe9dbe85a5f8de46d6abebd1ea2
chiaki-taro <chias.926@gmail.com>
2024-05-21 16:56:45 +0900

    レベルモデルの作成

---
+++ app/models/level.rb
@@ -0,0 +1,14 @@
+class Level < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '---' },
+    { id: 1, name: '0：症状がない' },
+    { id: 2, name: '1：ほんの少し辛い' },
+    { id: 3, name: '2：少し辛い' },
+    { id: 4, name: '3：もっと辛い' },
+    { id: 5, name: '4：かなり辛い' },
+    { id: 6, name: '5：耐えられないくらい辛い' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :symptoms
+end
--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,3 +1,13 @@
 class Symptom < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :level
   belongs_to :disease
+
+  validates :level_id, numericality: { other_than: 0, message: "can't be blank"} 
+
+  with_options presence: true do
+    validates :symptoms
+    validates :time
+    validates :level_id
+  end
 end
