47e0853ac2e96d295d80e083268fdd128c7d6c6d
chiaki-taro <chias.926@gmail.com>
2024-05-21 12:09:56 +0900

    患者一覧ページへの遷移

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,22 @@
 class PatientsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+
+  def index
+    @patients = @user.patients.order(patient_name: :asc)
+  end
+
+  def new
+    @patient = @user.patients.new
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def patient_params
+    params.require(:patient).permit(:patient_name, :sex_id, :birthday)
+  end
 end
