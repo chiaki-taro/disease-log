7451942ba9fdb151904c5702be25acc203849033
chiaki-taro <chias.926@gmail.com>
2024-05-21 13:35:33 +0900

    新しい患者を登録する機能の実装

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -10,6 +10,15 @@ class PatientsController < ApplicationController
     @patient = @user.patients.new
   end
 
+  def create
+    @patient = @user.patients.new(patient_params)
+    if @patient.save
+      redirect_to user_patients_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_user
