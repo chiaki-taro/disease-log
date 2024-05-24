b6a5ff93edb6d292f5107aa48d2b1a1fe3d2a695
chiaki-taro <chias.926@gmail.com>
2024-05-24 13:12:57 +0900

    パンくずリスト作成

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -7,27 +7,43 @@ crumb :patients do |user|
   parent :root
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
+crumb :patient_show do |user, patient|
+  link "#{patient.patient_name}さん", user_patient_path(user, patient)
+  parent :patients, user
+end
 
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
+crumb :patient_new do |user|
+  link "患者登録", new_user_patient_path(user)
+  parent :patients, user
+end
 
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
+crumb :patient_edit do |user, patient|
+  link "#{patient.patient_name}さんの編集", edit_user_patient_path(user, patient)
+  parent :patient_show, user, patient
+end
 
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
+crumb :disease_new do |user, patient|
+  link "#{patient.patient_name}さんの疾患登録", new_user_patient_disease_path(user, patient)
+  parent :patient_show, user, patient
+end
 
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
+crumb :disease_show do |user, patient, disease|
+  link "#{disease.disease_name}", user_patient_disease_path(user, patient, disease)
+  parent :patient_show, user, patient
+end
+
+
+crumb :disease_edit do |user, patient, disease|
+  link "#{disease.disease_name}編集", edit_user_patient_disease_path(user, patient, disease)
+  parent :disease_show, user, patient, disease
+end
+
+crumb :symptom_new do |user, patient, disease|
+  link "症状を入力", new_user_patient_disease_symptom_path(user, patient, disease)
+  parent :disease_show, user, patient, disease
+end
+
+crumb :symptom_edit do |user, patient, disease, symptom|
+  link "#{symptom.symptoms}を編集", edit_user_patient_disease_symptom_path(user, patient, disease, symptom)
+  parent :disease_show, user, patient, disease
+end
