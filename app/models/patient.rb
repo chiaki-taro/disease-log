be9b378131eda07ba7e6b55f9e57cb458b92b099
chiaki-taro <chias.926@gmail.com>
2024-05-27 13:05:46 +0900

    アソシエーション修正

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,7 @@ class Patient < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :sex
   belongs_to :user
-  has_many :diseases
+  has_many :diseases, dependent: :destroy
 
   validates :sex_id, numericality: { other_than: 0, message: "can't be blank"} 
 
