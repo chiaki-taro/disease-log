b1d632f28f01f9519d29c3e473d504b54f928f84
chiaki-taro <chias.926@gmail.com>
2024-06-12 15:58:03 +0900

    userモデルの単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    user_name                    { Faker::Name.name }
+    email                        { Faker::Internet.email }
+    password                     { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation        { password }
   end
 end
