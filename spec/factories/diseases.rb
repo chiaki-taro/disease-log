7bb5dc275702b3d3cf9cca7876e55716c88c4f8c
chiaki-taro <chias.926@gmail.com>
2024-06-19 16:35:18 +0900

    病気登録テスト

--- spec/factories/diseases.rb
+++ spec/factories/diseases.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :disease do
-    
+    association :patient
+
+    disease_name      { Faker::Name.name }
+    date_of_onset     { Faker::Time.between(from: DateTime.now - 7, to: DateTime.now) }
+    status_id         { Faker::Number.between(from: 1, to: 2) }
   end
 end
--- spec/models/disease_spec.rb
+++ spec/models/disease_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Disease, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @disease = FactoryBot.build(:disease)
+  end
+
+  describe '疾患新規登録' do
+    context '疾患新規登録できる場合' do
+      it '正常に登録できる' do
+        expect(@disease).to be_valid
+      end
+
+      it '発症日時を登録していれば登録できる' do
+        @disease.date_of_onset = Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+        expect(@disease).to be_valid
+      end
+
+      it 'ステータスを観察中・観察終了のいずれかを選択していれば登録できる' do
+        @disease.status_id = 1
+        expect(@disease).to be_valid
+      end
+    end
+
+    context '疾患新規登録出来ない時' do
+      it 'が空では登録できない' do
+        @disease.disease_name = ''
+        @disease.valid?
+        expect(@disease.errors.full_messages).to include("Disease name can't be blank")
+      end
+
+      it '発症日が空だと登録できない' do
+        @disease.date_of_onset = ''
+        @disease.valid?
+        expect(@disease.errors.full_messages).to include("Date of onset can't be blank")
+      end
+
+      it 'ステータスが---だと登録できない' do
+        @disease.status_id = 0
+        @disease.valid?
+        expect(@disease.errors.full_messages).to include("Status can't be blank")
+      end
+    end
+  end
 end
