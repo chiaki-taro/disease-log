16462a9eeb96c9c3f0c944b5297aaa9408e0d4eb
chiaki-taro <chias.926@gmail.com>
2024-06-19 17:12:13 +0900

    symptomテスト

--- spec/factories/symptoms.rb
+++ spec/factories/symptoms.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :symptom do
-    
+    association :disease
+
+    symptoms      { "咳" }
+    time          { Faker::Time.between(from: DateTime.now - 7, to: DateTime.now) }
+    level_id      { Faker::Number.between(from: 1, to: 6) }
+    treatment     { "薬を飲んだ" }
+    evaluation    { "薬の効果はあった" }
+    memo          { "薬で眠くなった" }
   end
 end
