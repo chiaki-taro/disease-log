f6c2f788a8a10ca220634a7f91f6560dce70de7d
chiaki-taro <chias.926@gmail.com>
2024-06-19 13:06:42 +0900

    患者モデルの単体テスト

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :patient do
-    
+    association :user
+
+    patient_name      { Faker::Name.name }
+    sex_id            { Faker::Number.between(from: 1, to: 3) }
+    birthday          { Faker::Date.backward(days: 365 * 100) }
   end
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Patient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @patient = FactoryBot.build(:patient)
+  end
+
+  describe '患者新規登録' do
+    context '患者新規登録できる場合' do
+      it '正常に登録できる' do
+        expect(@patient).to be_valid
+      end
+
+      it '性別が女性・男性・その他のいずれかを選択していれば登録できる' do
+        @patient.sex_id = 1
+        expect(@patient).to be_valid
+      end
+    end
+
+    context '患者新規登録出来ない時' do
+      it 'patient_nameが空では登録できない' do
+        @patient.patient_name = ''
+        @patient.valid?
+        expect(@patient.errors.full_messages).to include("Patient name can't be blank")
+      end
+
+      it '性別が空だと登録できない' do
+        @patient.sex_id = 0
+        @patient.valid?
+        expect(@patient.errors.full_messages).to include("Sex can't be blank")
+      end
+
+      it '誕生日が空だと登録できない' do
+        @patient.birthday = ''
+        @patient.valid?
+        expect(@patient.errors.full_messages).to include("Birthday can't be blank")
+      end
+    end
+  end
 end
